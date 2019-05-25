e85e28be5ab4cee678f38338a2ac14d01ebaa27b
whywhywhywhygo <whywhywhywhygo@gmail.com>
2019-05-25 17:30:33 +0800

    添加丢失文件

---
+++ Errors/TypeError.hpp
@@ -0,0 +1,33 @@
+//
+// Created by 17271 on 2019/5/25.
+//
+
+#ifndef CODEREFACTOR_TYPEERROR_H
+#define CODEREFACTOR_TYPEERROR_H
+
+#include <string>
+#include <exception>
+
+/// @brief Throw this while type specifiers are illegal
+class TypeError : public std::exception {
+public:
+	explicit TypeError(const char *error)
+			: m_msg{error}
+	{}
+	explicit TypeError(std::string error)
+	: m_msg{error.data()}
+	{}
+
+	~TypeError() override = default;
+
+	const char *what() const noexcept override
+	{
+		return m_msg;
+	}
+
+private:
+	const char *m_msg;
+};
+
+
+#endif //CODEREFACTOR_TYPEERROR_H
